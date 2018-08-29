54d360362d50de97a66e8d493b9b5ecba4b3f187
Sharvari desai <sharvari.woxi@gmail.com>
2018-08-29 13:49:04 +0530

    Message listing changes

---
+++ app/src/main/java/com/woxi/sgks_member/home/MessageHomeNewFragment.java
@@ -0,0 +1,27 @@
+package com.woxi.sgks_member.home;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.woxi.sgks_member.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MessageHomeNewFragment extends Fragment {
+    public MessageHomeNewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+}
