7b39f68fea39adc15fc4d49218a45c47215a0a9d
mayur-woxi <mayur.woxi@gmail.com>
2018-12-11 15:25:08 +0530

    added badges to messages and classified

--- app/src/main/java/com/woxi/sgkks_member/home/HomeActivity.java
+++ app/src/main/java/com/woxi/sgkks_member/home/HomeActivity.java
@@ -15,6 +15,7 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -46,6 +47,7 @@ import com.woxi.sgkks_member.utils.AppSettings;
 import com.woxi.sgkks_member.utils.LocaleHelper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static com.woxi.sgkks_member.interfaces.AppConstants.CURRENT_PAGE;
 import static com.woxi.sgkks_member.interfaces.AppConstants.LANGUAGE_ENGLISH;
@@ -250,7 +252,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    AppCommonMethods.putStringPref(AppConstants.PREFS_LOCAL_MESSAGE_ID, arrLocalMessageIds.toString(), mContext);
+//                    AppCommonMethods.putStringPref(AppConstants.PREFS_LOCAL_MESSAGE_ID, arrLocalMessageIds.toString(), mContext);
                 }
                 if (position == intClassifiedTabIndex && isClassifiedCountApplied) {
                     badgeClassifiedTab.setVisibility(View.GONE);
@@ -259,7 +261,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    AppCommonMethods.putStringPref(AppConstants.PREFS_LOCAL_CLASSIFIED_ID, arrLocalClassifiedIds.toString(), mContext);
+//                    AppCommonMethods.putStringPref(AppConstants.PREFS_LOCAL_CLASSIFIED_ID, arrLocalClassifiedIds.toString(), mContext);
                 }
                 if (position == 2){
                     mFabAddNewMember.setVisibility(View.GONE);
@@ -278,6 +280,13 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             AppCommonMethods.putBooleanPref(AppConstants.PREFS_IS_LOCAL_STORAGE_SYNC_ENABLED, true, mContext);
             AppCommonMethods.putBooleanPref(AppConstants.PREFS_IS_OFFLINE_SUPPORT_ENABLED, true, mContext);
             AppCommonMethods.putBooleanPref(APP_DATABASE_CREATED, true, mContext);
+        }
+
+        if (new AppCommonMethods(mContext).isNetworkAvailable()) {
+            //Setting message count on Message Tab
+            setMessageCount(mTabLayout);
+            //Setting classified count on Message Tab
+            setClassifiedCount(mTabLayout);
         }
 //        boolean isLocalStorageSyncEnabled = AppCommonMethods.getBooleanPref(AppConstants.PREFS_IS_LOCAL_STORAGE_SYNC_ENABLED, mContext);
 //        if (isLocalStorageSyncEnabled) {
@@ -393,11 +402,113 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-
-
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(LocaleHelper.onAttach(newBase));
+    }
+
+    private void setClassifiedCount(TabLayout mTabLayout) {
+        //Get locally stored Message ids.
+        /*arrLocalClassifiedIds = new ArrayList<>();
+        String strLocalClassifiedIds = AppCommonMethods.getStringPref(AppConstants.PREFS_LOCAL_CLASSIFIED_ID, mContext);
+        strLocalClassifiedIds = strLocalClassifiedIds.replace("[", "");
+        strLocalClassifiedIds = strLocalClassifiedIds.replace("]", "");
+        arrLocalClassifiedIds = new ArrayList<>(Arrays.asList(strLocalClassifiedIds.split(",")));
+
+        //Get latest (from server) news ids.
+        arrClassifiedIds = new ArrayList<>();
+        String strClassifiedIds = AppCommonMethods.getStringPref(AppConstants.PREFS_LATEST_CLASSIFIED_ID, mContext);
+        strClassifiedIds = strClassifiedIds.replace("[", "");
+        strClassifiedIds = strClassifiedIds.replace("]", "");
+        arrClassifiedIds = new ArrayList<>(Arrays.asList(strClassifiedIds.split(",")));
+        ArrayList<String> arrTempLatestClassified = new ArrayList<>(Arrays.asList(strClassifiedIds.split(",")));*/
+
+        /**
+         * 1. get all local Classified ids in arraylist - arrLocalClassifiedIds
+         * 2. get latest Classified ids in arraylist - arrClassifiedIds, arrTempLatest
+         * 3. compare local id with latest id
+         * 4. if local id equal to latest id then remove that id from latest ids array i.e. - arrClassifiedIds
+         * 5. Now get size of arrClassifiedIds array which is count to be shown
+         * 6. Later on selection of news tab add all items from arrClassifiedIds array to arrLocalClassifiedIds
+         */
+      /*  for (String strLocalClassifiedId : arrLocalClassifiedIds) {
+            strLocalClassifiedId = strLocalClassifiedId.replace("\"", "");
+            strLocalClassifiedId = strLocalClassifiedId.replace(" ", "");
+            for (String strLatestId : arrTempLatestClassified) {
+                String strTemp = strLatestId;
+                strLatestId = strLatestId.replace("\"", "");
+//                strLatestId = strLatestId.replace(" ", "");
+                if (strLocalClassifiedId.equalsIgnoreCase(strLatestId)) {
+                    try {
+                        arrClassifiedIds.remove(strTemp);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        if (arrClassifiedIds.size() > 0 && !arrClassifiedIds.get(0).equalsIgnoreCase("")) {*/
+            View view = LayoutInflater.from(this).inflate(R.layout.badge_layout_tab_count, null);
+            badgeClassifiedTab = (TextView) view.findViewById(R.id.tvTabBadge);
+            ImageView ivTabIcon = (ImageView) view.findViewById(R.id.ivTabIcon);
+            ivTabIcon.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_classified, null));
+//            badgeClassifiedTab.setText(String.valueOf(arrClassifiedIds.size()));
+            badgeClassifiedTab.setText("12");
+            mTabLayout.getTabAt(intClassifiedTabIndex).setCustomView(view);
+            isClassifiedCountApplied = true;
+        //}
+    }
+
+    private void setMessageCount(TabLayout mTabLayout) {
+        //Get locally stored Message ids.
+       /* arrLocalMessageIds = new ArrayList<>();
+        String strLocalMessageIds = AppCommonMethods.getStringPref(AppConstants.PREFS_LOCAL_MESSAGE_ID, mContext);
+        strLocalMessageIds = strLocalMessageIds.replace("[", "");
+        strLocalMessageIds = strLocalMessageIds.replace("]", "");
+        arrLocalMessageIds = new ArrayList<>(Arrays.asList(strLocalMessageIds.split(",")));
+
+        //Get latest (from server) news ids.
+        arrMessageIds = new ArrayList<>();
+        String strMessageIds = AppCommonMethods.getStringPref(AppConstants.PREFS_LATEST_MESSAGE_ID, mContext);
+        strMessageIds = strMessageIds.replace("[", "");
+        strMessageIds = strMessageIds.replace("]", "");
+        arrMessageIds = new ArrayList<>(Arrays.asList(strMessageIds.split(",")));
+        ArrayList<String> arrTempLatest = new ArrayList<>(Arrays.asList(strMessageIds.split(",")));*/
+
+        /**
+         * 1. get all local Message ids in arraylist - arrLocalMessageIds
+         * 2. get latest Message ids in arraylist - arrMessageIds, arrTempLatest
+         * 3. compare local id with latest id
+         * 4. if local id equal to latest id then remove that id from latest ids array i.e. - arrMessageIds
+         * 5. Now get size of arrMessageIds array which is count to be shown
+         * 6. Later on selection of news tab add all items from arrMessageIds array to arrLocalMessageIds
+         */
+       /* for (String strLocalMessageId : arrLocalMessageIds) {
+            strLocalMessageId = strLocalMessageId.replace("\"", "");
+            strLocalMessageId = strLocalMessageId.replace(" ", "");
+            for (String strLatestId : arrTempLatest) {
+                String strTemp = strLatestId;
+                strLatestId = strLatestId.replace("\"", "");
+//                strLatestId = strLatestId.replace(" ", "");
+                if (strLocalMessageId.equalsIgnoreCase(strLatestId)) {
+                    try {
+                        arrMessageIds.remove(strTemp);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        if (arrMessageIds.size() > 0 && !arrMessageIds.get(0).equalsIgnoreCase("")) {*/
+            View view = LayoutInflater.from(this).inflate(R.layout.badge_layout_tab_count, null);
+            badgeMessageTab = (TextView) view.findViewById(R.id.tvTabBadge);
+            ImageView ivTabIcon = (ImageView) view.findViewById(R.id.ivTabIcon);
+            ivTabIcon.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_messages, null));
+            //badgeMessageTab.setText(String.valueOf(arrMessageIds.size()));
+        badgeMessageTab.setText("10");
+            mTabLayout.getTabAt(intMessageTabIndex).setCustomView(view);
+            isMessageCountApplied = true;
+       // }
     }
 
 
